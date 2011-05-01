2d03aba85e6f1d2bc8c2b9a8d6901c0567b57a26
Ivan Necas <necasik@gmail.com>
2011-05-01 13:19:14 +0200

    Support for examples from other gherkin files
    With respect to 'https://github.com/aslakhellesoy/gherkin/wiki/Code-Completion',
    each step regexp can be provided with examples from gherkin files. This examples
    are matched against the entered text as well.
    
    Filtering of examples works as follows:
    If the regexp string matches the input, all examples are suggested.
    Otherwise only matching examples are included. (more in spec file).

--- spec/autocomplete.spec.js
+++ spec/autocomplete.spec.js
@@ -21,23 +21,46 @@ var Autocomplete = require('autocomplete/autocomplete').Autocomplete;
 describe("Steps Autocompletion", function(){
   describe("on simple step defintions", function () {
     beforeEach(function(){
-      var stepDefinitions = [
-        '^I press "([^"]*)"$',
-        '^he presses button$',
-        '^I follow "([^"]*)"$'
-      ];
+      var stepDefinitions = {
+        '^I press "([^"]*)"$' : ['I press "Apply"','I press "Save"'],
+        '^he presses button$' : [],
+        '^I am on ([^"]*)$' : ['I am on home page', 'I am on login page']
+      };
       autocomplete = new Autocomplete(stepDefinitions);
     });
 
     it("suggest suitable steps for entered text", function(){
       var expectedSuggestions = [
         'I press "([^"]*)"',
+        '** I press "Apply"',
+        '** I press "Save"',
         'he presses button'];
       var suggestedSteps = autocomplete.suggest("pre");
-      var suggestedStepNames = suggestedSteps.map(function (stepDefinition) {
-        return stepDefinition.step();
+      var suggestedStepNames = [];
+      suggestedSteps.forEach(function (stepDefinition) {
+        suggestedStepNames.push(stepDefinition.step());
+        stepDefinition.examples.forEach(function (example) {
+          suggestedStepNames.push("** "+example);
+        });
       });
       expect(suggestedStepNames).toBeArray(expectedSuggestions);
+    });
+
+    describe("Entered text matches an example", function () {
+      it("suggests this example, including it's step definition", function () {
+      var expectedSuggestions = [
+        'I press "([^"]*)"',
+        '** I press "Apply"'];
+      var suggestedSteps = autocomplete.suggest('I press "App');
+      var suggestedStepNames = [];
+      suggestedSteps.forEach(function (stepDefinition) {
+        suggestedStepNames.push(stepDefinition.step());
+        stepDefinition.examples.forEach(function (example) {
+          suggestedStepNames.push("** "+example);
+        });
+      });
+      expect(suggestedStepNames).toBeArray(expectedSuggestions);
+      });
     });
   });
 });
