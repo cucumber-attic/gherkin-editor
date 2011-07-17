f6b934ce7dd7f1cf2bc454934daa539fba3360b6
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-07-17 13:45:16 +0100

    Fix indentation

--- public/applet/gherkin/editor/PartialMatch.java
+++ public/applet/gherkin/editor/PartialMatch.java
@@ -5,15 +5,15 @@ import java.util.regex.*;
 
 // http://stackoverflow.com/questions/2469231/how-can-i-perform-a-partial-match-with-java-util-regex
 public class PartialMatch extends Applet {
-	// TODO: return an enum: NO_MATCH | EXACT_MATCH | PARTIAL_MATCH
-	// Might be useful for autocomplete to make better decisions.
-  public static boolean isPartialMatch(String regexp, String text) {
-		Pattern p = Pattern.compile(regexp);
-		Matcher m = p.matcher(text);
-		return m.matches() || m.hitEnd();
-  }
+    // TODO: return an enum: NO_MATCH | EXACT_MATCH | PARTIAL_MATCH
+    // Might be useful for autocomplete to make better decisions.
+    public static boolean isPartialMatch(String regexp, String text) {
+        Pattern p = Pattern.compile(regexp);
+        Matcher m = p.matcher(text);
+        return m.matches() || m.hitEnd();
+    }
 
-	public static void main(String[] args) {
-		System.out.println(isPartialMatch(args[0], args[1]));
-	}
+    public static void main(String[] args) {
+        System.out.println(isPartialMatch(args[0], args[1]));
+    }
 }
